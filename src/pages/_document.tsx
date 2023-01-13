9df0b5baaebda6ffbbe627d0a44089f9ac81b5ea
Leonardo Silva <silvaleonardo.ti@gmail.com>
2023-01-13 12:02:07 -0300

    setting page tittle and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>GoPomodoro</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
