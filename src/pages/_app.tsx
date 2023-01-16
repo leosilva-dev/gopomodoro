1064ac63846abe6ec5c567aa8fcaf8aba53cffd7
Leonardo Silva <silvaleonardo.ti@gmail.com>
2023-01-16 17:50:41 -0300

    alterando local da tag <Head />

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>GoPomodoro</title>
+        <link rel="shortcut icon" href="./favicon.png" />
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>GoPomodoro</title>
-        <link rel="shortcut icon" href="/favicon.png" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
