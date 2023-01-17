296b4f97f10af0443616450953aaee681f2ef1e4
Leonardo Silva <silvaleonardo.ti@gmail.com>
2023-01-17 16:35:57 -0300

    Improving layout

--- src/components/pomodoro/Pomodoro.tsx
+++ src/components/pomodoro/Pomodoro.tsx
@@ -1,14 +1,12 @@
 import { VStack, Button, Icon, Box, Text } from "@chakra-ui/react";
-import { ArrowClockwise, Pause, Play } from "phosphor-react";
+import { ArrowClockwise, Pause, Play, Stop } from "phosphor-react";
+import { ToggleMode } from "../toggle-mode/ToggleMode";
 import { usePomo } from "../../hooks/usePomo";
-import { ConfigModal } from "../config-modal/ConfigModal";
-import { BackgroundSound } from "../sound/BackgroundSound";
 
 export default function Pomodoro() {
   const {
     isCounting,
     startPomo,
-    stopPomo,
     startNextPomo,
     getClockLabel,
     setTypePomo,
@@ -18,80 +16,78 @@ export default function Pomodoro() {
   } = usePomo();
 
   return (
-    <VStack
-      h="100vh"
-      display={"flex"}
-      alignItems="center"
-      justifyContent={"center"}
-    >
-      <Box marginTop={2}>
-        <Button
-          marginRight={2}
-          color="primary"
-          variant={pomoType === "pomo" ? "solid" : "ghost"}
-          onClick={() => setTypePomo("pomo")}
-        >
-          Pomodoro
-        </Button>
-        <Button
-          marginRight={2}
-          color="primary"
-          variant={pomoType === "short-break" ? "solid" : "ghost"}
-          onClick={() => setTypePomo("short-break")}
-        >
-          Short break
-        </Button>
-        <Button
-          marginRight={2}
-          color="primary"
-          variant={pomoType === "long-break" ? "solid" : "ghost"}
-          onClick={() => setTypePomo("long-break")}
-        >
-          Long break
-        </Button>
+    <>
+      <Box display={"flex"} justifyContent="flex-end" margin={2}>
+        <ToggleMode />
       </Box>
-      <Box>
-        <Text fontSize="150px">{getClockLabel()}</Text>
-      </Box>
-      <Box paddingBottom={5}>
-        {isCounting && (
+      <VStack
+        h="100vh"
+        display={"flex"}
+        alignItems="center"
+        justifyContent={"center"}
+      >
+        <Box marginTop={2}>
           <Button
-            leftIcon={<Icon as={Pause} />}
+            marginRight={2}
             color="primary"
-            variant="solid"
-            onClick={stopPomo}
+            variant={pomoType === "pomo" ? "solid" : "ghost"}
+            onClick={() => setTypePomo("pomo")}
           >
-            Pause Pomo
+            Pomodoro
           </Button>
-        )}
-        {!isCounting && !isPomoFinished && (
           <Button
-            leftIcon={<Icon as={Play} />}
+            marginRight={2}
             color="primary"
-            variant="solid"
-            onClick={startPomo}
+            variant={pomoType === "short-break" ? "solid" : "ghost"}
+            onClick={() => setTypePomo("short-break")}
           >
-            Start Pomo
+            Short break
           </Button>
-        )}
-        {!isCounting && isPomoFinished && (
           <Button
-            leftIcon={<Icon as={Play} />}
+            marginRight={2}
             color="primary"
-            variant="solid"
-            onClick={startNextPomo}
+            variant={pomoType === "long-break" ? "solid" : "ghost"}
+            onClick={() => setTypePomo("long-break")}
           >
-            Start next Pomo
+            Long break
           </Button>
-        )}
-      </Box>
-      <Box>
-        <ConfigModal />
-        <Button onClick={restartPomo} variant="link">
-          <ArrowClockwise size={25} weight="light" />
-        </Button>
-        <BackgroundSound />
-      </Box>
-    </VStack>
+        </Box>
+        <Box>
+          <Text fontSize="150px">{getClockLabel()}</Text>
+        </Box>
+        <Box paddingBottom={5}>
+          {isCounting && (
+            <Button
+              leftIcon={<Icon as={ArrowClockwise} />}
+              color="primary"
+              variant="solid"
+              onClick={restartPomo}
+            >
+              Restart Pomo
+            </Button>
+          )}
+          {!isCounting && !isPomoFinished && (
+            <Button
+              leftIcon={<Icon as={Play} />}
+              color="primary"
+              variant="solid"
+              onClick={startPomo}
+            >
+              Start Pomo
+            </Button>
+          )}
+          {!isCounting && isPomoFinished && (
+            <Button
+              leftIcon={<Icon as={Play} />}
+              color="primary"
+              variant="solid"
+              onClick={startNextPomo}
+            >
+              Start next Pomo
+            </Button>
+          )}
+        </Box>
+      </VStack>
+    </>
   );
 }
